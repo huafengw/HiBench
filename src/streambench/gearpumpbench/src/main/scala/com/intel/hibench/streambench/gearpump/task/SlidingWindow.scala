5277c1cc122f179ca001e94f8731dfba245c6e32
huafengw <fvunicorn@gmail.com>
2016-09-19 14:59:54 +0800

    minor

--- src/streambench/gearpumpbench/src/main/scala/com/intel/hibench/streambench/gearpump/task/SlidingWindow.scala
+++ src/streambench/gearpumpbench/src/main/scala/com/intel/hibench/streambench/gearpump/task/SlidingWindow.scala
@@ -29,6 +29,48 @@ class SlidingWindow(taskContext: TaskContext, conf: UserConfig) extends Task(tas
   private val windowStep = benchConfig.windowSlideStep
   import taskContext.output
 
-  override def onNext(msg: Message): Unit = {
+  // windowStartTime -> (ip -> (minMessageTime, count))
+  private val windowCounts = new TreeSortedMap[Long, UnifiedMap[String, (TimeStamp, Long)]]
+
+  override def onNext(message: Message): Unit = {
+    val ip = message.msg.asInstanceOf[String]
+    val msgTime = message.timestamp
+    getWindows(msgTime).foreach { window =>
+      val countsByIp = windowCounts.getOrDefault(window, new UnifiedMap[String, (TimeStamp, Long)])
+      val (minTime, count) = countsByIp.getOrDefault(ip, (msgTime, 0L))
+      countsByIp.put(ip, (Math.min(msgTime, minTime), count + 1L))
+      windowCounts.put(window, countsByIp)
+    }
+
+    val reporter = new KafkaReporter(benchConfig.reporterTopic, benchConfig.brokerList)
+    var windowStart = windowCounts.firstKey()
+    while (taskContext.upstreamMinClock >= (windowStart + windowDuration)) {
+      val countsByIp = windowCounts.remove(windowStart)
+      countsByIp.forEachValue(new Procedure[(TimeStamp, Long)]() {
+
+        override def value(tuple: (TimeStamp, Long)): Unit = {
+          assert(tuple._1 >= windowStart && tuple._1 <= (windowStart + windowDuration) )
+          (1 to tuple._2.toInt).foreach(i =>reporter.report(tuple._1, System.currentTimeMillis()))
+        }
+      })
+      windowStart = windowCounts.firstKey()
+    }
+  }
+
+
+  private def getWindows(timestamp: TimeStamp): List[TimeStamp] = {
+    val windows = ArrayBuffer.empty[TimeStamp]
+    var start = lastStartFor(timestamp)
+    windows += start
+    start -= windowStep
+    while (start >= timestamp) {
+      windows += start
+      start -= windowStep
+    }
+    windows.toList
+  }
+
+  private def lastStartFor(timestamp: TimeStamp): TimeStamp = {
+    timestamp - (timestamp + windowStep) % windowStep
   }
 }
